3d236d4b617c8d06c8dcd9116a908ca02d2cfebe
SOL X <john.forfar@gmail.com>
2023-04-18 22:21:51 +1000

    environment variables setup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const userRouter = require("./routes/userController.js");
 const helloMiddleware = require("./middleware/helloMiddleware.js");
+const variables = require("./config/variables.js");
+console.log(variables.username);
 app.use(helloMiddleware);
 app.use(express.json());
 app.listen(3000);
